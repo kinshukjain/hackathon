1ad4d2e5894629d7283ef40a52413a7763bae371
kinshuk <kinshuk@sumologic.com>
2018-02-23 22:21:14 +0530

    updating comments

--- app/routes/concierge_routes.js
+++ app/routes/concierge_routes.js
@@ -3,7 +3,11 @@ const conciergeData = require('../constants').conciergeData;
 module.exports = function(app) {
   // post call for concierge
   app.post('/giveMeJPG', (req, res) => {
+    //  conciergeData.setData(req.body);
+    //  call chromeless
+    //  render dashboard
+    //  get snapshot
+    //  res.send(jpg file);
     console.log(req.body);
-    // conciergeData.setData(req.body);
   });
 };
