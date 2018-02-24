9f860a99ae30c8e4d9669f4fa5eedab711862cf6
kinshuk <kinshuk@sumologic.com>
2018-02-24 14:10:47 +0530

    final commit with lambda up and running

--- app/routes/concierge_routes.js
+++ app/routes/concierge_routes.js
@@ -3,11 +3,15 @@ const chromeless = require('../utils').chromeless;
 
 module.exports = function (app) {
   // post call for concierge
-  app.get('/giveMeJPG', (req, res) => {
+  app.get('/give-me-jpg', (req, res) => {
     //  conciergeData.setData(req.body);
     //  render dashboard and get screenshot
-    chromeless.getScreenshot().then(screenshot => {
-      res.send(screenshot);
+    chromeless.getScreenshot()
+    .then(screenshot => {
+      res.status(200).send(screenshot);
+    })
+    .catch(err => {
+      res.status(400).send(err);
     });
     // console.log(req.body);
   });
--- app/utils/chromeless.js
+++ app/utils/chromeless.js
@@ -24,3 +24,26 @@ exports.getScreenshot = (url = 'https://www.google.com') => {
   };
   return run().catch(console.error.bind(console));
 };
+
+//exports.getImageFromPath = (path) => {
+//  var img = ''
+//    , fs = require('fs');
+//
+//  fs.readFile(path, function(err, data) {
+//    if (err) throw err; // Fail if the file can't be read.
+//    img = data; // Send the file data to the browser.
+//  });
+//  return img;
+//};
+//
+//exports.getImageFromUrl = async (url) => {
+//  var request = require('request').defaults({ encoding: null });
+//  return await request.get(url)
+//  .on('response', function (res) {
+//    if (res.statusCode === 200) {
+//      return res.body;
+//    }
+//    return '';
+//  })
+//  .on('error', function(err){ throw err; });
+//};
